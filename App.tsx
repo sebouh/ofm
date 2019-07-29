34c9bb01c99cc5e938fe7f58be59f6cc12f15ac6
Ashot Israelyan <ashotisraelyan@gmail.com>
2019-07-29 11:08:45 +0400

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import Camera from './src/components/Camera';
+
+const App = () => {
+  return <Camera />;
+};
+
+export default App;
